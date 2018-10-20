b123d51f95a30a95d839d04882fb67fb8c3b8197
Owen-Krueger <owk914@gmail.com>
2018-10-20 17:17:56 -0500

    Duluth Facts

--- duluthFacts.py
+++ duluthFacts.py
@@ -12,38 +12,38 @@ from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
 
 
-SKILL_NAME = "Titanic Facts"
-FACT_MESSAGE = "Here's your Titanic Fact: "
+SKILL_NAME = "Duluth Facts"
+FACT_MESSAGE = "Here's your Duluth Fact: "
 
 facts = [
-	'The Titanic was 269.1 meters or 882 feet and 9 inches long',
-	'The Titanic used 825 tons of coal a day',
-	'There were approximately 10,000 lamp bulbs used on the ship',
-	'The Titanic cost 7 mililon 5 hundred thousand dollars to make',
-	'Two workers were killed during the construction of the Titanic',
-	'Twenty horses were needed to transport the main anchor',
-	'There were 13 honeymooning couples on the maiden voyage of the Titanic',
-	'31.6 percent of passengers and crew survived',
-	'There were 2,223 people aboard the Titanic',
-	'The Titanic could hold a maximum of 3,547 people',
-	'The Titanic was equipped to hold 64 lifeboats',
-	'The Titanic was actually carrying 20 lifeboats',
-	'14,000 gallons of drinking water were used every 24 hours',
+	'In the late 1800s and early 1900s, the city of Duluth was home to more millionaires per capita than any other city in the world',
+	'1 out of ever 7 Duluth residents are employed in the medical field',
+	'Over 1000 ocean-going and freshwater ships use the Port of Duluth annually',
+	'The Duluth harbor is 2,342 miles from the Atlantic',
+	'On average, 40 million metric tons of cargo pass through the Twin Ports aboard 1,100 vessels each year',
+	'Minnesota Point is the longest fresh-water sandspit in the world',
+	'Duluth extends 24 miles along Lake Superior shores',
+	'The Lake Superior Zoo was established in 1923 under the city’s permission to construct a pen for a pet deer',
+	'Canal Park was historically used as an industrial dumping ground and also contained the area’s red light district.',
+	'From 1891 to 1939 Duluth had an extensive cable car system',
+	'In 1905 the Aerial Transfer Bridge was constructed, with a large gondola capable of carrying up to 60 tons of traffic',
+	'In 1929, the lift bridge in Duluth was modified into today’s Aerial Lift bridge',
+	'Chester Congdon donated funds for the City of Duluth to purchase the land along the eastern shoreline in order to preserve the area in its natural state and remain undeveloped',
 ]
 
 sb = SkillBuilder()
 
-class TitanicFactHandler(AbstractRequestHandler):
+class DuluthFactHandler(AbstractRequestHandler):
     
 	def can_handle(self, handler_input):
 		return(is_request_type("LaunchRequest")(handler_input) or
-				is_intent_name("NewTitanicFact")(handler_input))
+				is_intent_name("NewDuluthFact")(handler_input))
         
 	def handle(self, handler_input):
 		logging.info("Starting Default")
 
-		titanic_fact = random.choice(facts)
-		speech = FACT_MESSAGE + titanic_fact
+		duluth_fact = random.choice(facts)
+		speech = FACT_MESSAGE + duluth_fact
 
 		handler_input.response_builder.speak(speech).set_card(
 			SimpleCard(SKILL_NAME, speech))
@@ -55,7 +55,7 @@ class HelpHandler(AbstractRequestHandler):
 		return(is_intent_name("AMAZON.HelpIntent")(handler_input))
 		
 	def handle(self, handler_input):
-		speech = "You can ask me for a Titanic fact!"
+		speech = "You can ask me for a Duluth fact!"
 		
 		handler_input.response_builder.speak(speech).set_card(
             SimpleCard(SKILL_NAME, speech)).ask(speech)
@@ -72,7 +72,7 @@ class CancelOrStopHandler(AbstractRequestHandler):
         handler_input.response_builder.speak("Goodbye")
         return handler_input.response_builder.response		
 		
-sb.add_request_handler(TitanicFactHandler())
+sb.add_request_handler(DuluthFactHandler())
 sb.add_request_handler(HelpHandler())
 sb.add_request_handler(CancelOrStopHandler())
 
